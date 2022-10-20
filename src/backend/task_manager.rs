700312590d80cb6fe1f14be2957fa9200f372ded
Pau Murciano <pau.muciano@gameloft.com>
2022-10-20 19:27:34 +0200

    Add configurable states

--- src/backend/task_manager.rs
+++ src/backend/task_manager.rs
@@ -1,11 +1,12 @@
+use serde::{Deserialize, Serialize};
 use std::{fs::File, io::Write};
-use serde::{Serialize, Deserialize};
 
 use crate::backend::Task;
 
 #[derive(Serialize, Deserialize)]
 struct TasksState {
     tasks: Vec<Task>,
+    valid_states: Vec<String>,
 }
 
 pub struct TaskManager {
@@ -13,21 +14,29 @@ pub struct TaskManager {
 }
 
 impl TaskManager {
-
     pub fn new() -> TaskManager {
         TaskManager {
-            tasks_state : TasksState{tasks: Vec::new()},
+            tasks_state: TasksState {
+                tasks: Vec::new(),
+                valid_states: Vec::new(),
+            },
         }
     }
 
     pub fn initialize(&mut self) {
-        let open_file_result = File::open("state.json"); 
+        let open_file_result = File::open("state.json");
         if let Ok(file) = open_file_result {
             let tasks_state_from_json = serde_json::from_reader(file);
             if let Ok(tasks_state) = tasks_state_from_json {
                 self.tasks_state = tasks_state;
+                return;
             }
         }
+        //We could not initialize, we are going to add some default states
+        self.add_state("Open".to_string());
+        self.add_state("Selected for development".to_string());
+        self.add_state("In progress".to_string());
+        self.add_state("Done".to_string());
     }
 
     pub fn save(&self) {
@@ -44,13 +53,29 @@ impl TaskManager {
     }
 
     pub fn add_task(&mut self, i_task: Task) {
+        if !self.is_task_valid(&i_task) {
+            return;
+        }
         self.tasks_state.tasks.push(i_task);
         self.save();
     }
 
-    pub fn get_tasks(&self) -> &Vec<Task>
-    {
+    pub fn get_tasks(&self) -> &Vec<Task> {
         &self.tasks_state.tasks
     }
 
+    pub fn add_state(&mut self, state: String) {
+        if self.tasks_state.valid_states.contains(&state) {
+            return;
+        }
+        self.tasks_state.valid_states.push(state);
+    }
+
+    pub fn get_states(&self) -> &Vec<String> {
+        &self.tasks_state.valid_states
+    }
+
+    pub fn is_task_valid(&self, task: &Task) -> bool {
+        self.tasks_state.valid_states.contains(&task.state)
+    }
 }
